f6d9693dae43fa39d27f0685b831f386184d6138
fengkx <liangkx8237@gmail.com>
2021-07-30 08:30:34 +0800

    feat: loading at home page

---
+++ components/Loading.tsx
@@ -0,0 +1,55 @@
+import clsx from "clsx";
+import React from "react";
+
+type PropsType = {
+  style?: React.CSSProperties;
+  className?: string;
+};
+export const Loading: React.FC<PropsType> = ({ style, className }) => {
+  return (
+    <>
+      <style jsx>{`
+        .loader,
+        .loader:after {
+          border-radius: 50%;
+        }
+        .loader {
+          margin: 60px auto;
+          font-size: 10px;
+          position: relative;
+          text-indent: -9999em;
+          border-top: 1.1em solid rgba(64, 69, 79, 0.2);
+          border-right: 1.1em solid rgba(64, 69, 79, 0.2);
+          border-bottom: 1.1em solid rgba(64, 69, 79, 0.2);
+          border-left: 1.1em solid white;
+          -webkit-transform: translateZ(0);
+          -ms-transform: translateZ(0);
+          transform: translateZ(0);
+          -webkit-animation: load8 1.1s infinite linear;
+          animation: load8 1.1s infinite linear;
+        }
+        @-webkit-keyframes load8 {
+          0% {
+            -webkit-transform: rotate(0deg);
+            transform: rotate(0deg);
+          }
+          100% {
+            -webkit-transform: rotate(360deg);
+            transform: rotate(360deg);
+          }
+        }
+        @keyframes load8 {
+          0% {
+            -webkit-transform: rotate(0deg);
+            transform: rotate(0deg);
+          }
+          100% {
+            -webkit-transform: rotate(360deg);
+            transform: rotate(360deg);
+          }
+        }
+      `}</style>
+      <div className={clsx("loader", className)} style={style} />
+    </>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,17 @@ import { useUpdateAtom } from "jotai/utils";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import { gotoPageAtom } from "../components/Editor/adapters/memory";
+import { Loading } from "../components/Loading";
 const Home: NextPage = () => {
   const router = useRouter();
   const gotoPage = useUpdateAtom(gotoPageAtom);
   useMountEffect(() => {
     gotoPage({ router, today: true });
   });
-  return null;
+  return (
+    <div className="grid place-items-center h-screen">
+      <Loading className=" w-64 h-64" />
+    </div>
+  );
 };
 export default Home;
