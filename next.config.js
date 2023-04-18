adfa403bec4b334a8e4944bd36bd0162ce68c96b
fengkx <liangkx8237@gmail.com>
2023-04-18 23:02:09 +0800

    chore: worker

--- next.config.js
+++ next.config.js
@@ -8,5 +8,6 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 module.exports = withBundleAnalyzer({
   experimental: {
     esmExternals: true,
+    webpackBuildWorker: true,
   },
 });
