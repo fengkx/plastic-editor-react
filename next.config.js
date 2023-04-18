4968bcb0af87b3c388933120b937f3f8b76b6134
fengkx <liangkx8237@gmail.com>
2023-04-18 21:55:10 +0800

    chore: update deps

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
+
+/**
+ * @type {import('next').NextConfig}
+ */
 module.exports = withBundleAnalyzer({
   experimental: {
     reactRoot: true,
-    esmExternals: "true",
+    esmExternals: true,
   },
 });
